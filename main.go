e5926afd6ca541fa3ebacab88deb65cade0d03b8
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-09-25 07:54:17 -0600

    refactor: streamline main.go by removing health check endpoint and integrating GetAllOrganizations handler

--- main.go
+++ main.go
@@ -4,18 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"samla-admin/api"
 )
 
 func main() {
-	// Basic HTTP server setup
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to Samla Admin!")
 	})
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "OK")
-	})
+	http.HandleFunc("/organizations", api.GetAllOrganizations) // GET: All organizations
 
 	port := ":8080"
 	fmt.Printf("Server starting on port %s\n", port)
