ced5d82383596af10b5304e80034f059cdfd7acc
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-09-25 07:54:05 -0600

    feat: add VerifyMethod function to validate HTTP request methods

---
+++ api/utils.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"net/http"
+	"strings"
+)
+
+func VerifyMethod(r *http.Request, allowedMethods []string) bool {
+	for _, method := range allowedMethods {
+		if r.Method == strings.ToUpper(method) {
+			return true
+		}
+	}
+	return false
+}
